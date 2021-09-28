dae6ee28e9473a10eb61f1076cfad14f5882008f
Carl <carlbrainegebe@gmail.com>
2021-09-28 17:27:06 -0500

    add login route and action for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,30 @@ class ApplicationController < ActionController::API
   include ActionController::Cookies
 
   rescue_from ActiveRecord::RecordNotFound, with: :record_invalid_response
- 
+  before_action :authorize
+
+  def authorize
+    authorization_header = request.headers["authorization"]
+    # byebug
+    
+
+    if !authorization_header
+      render json: { error: "No authorization header" }, status: :unauthorized
+    else
+      token = authorization_header.split(" ")[1]
+      secret = Rails.application.secret_key_base
+      
+      begin
+          payload = JWT.decode(token, secret).first 
+          user = User.find(payload["user_id"])
+      rescue
+        render json: { errors: ["Unauthorized, bad token"], status: :unauthorized }
+      end
+    end
+
+    # user = User.find_by(id: session[:user_id])
+    # render json: { errors: ["Not authorized"] }, status: :unauthorized unless user
+  end
 
   private
 
