6897df8dd1775792d9545ab066a90a7a69e1ff56
Carl <carlbrainegebe@gmail.com>
2021-09-28 17:29:14 -0500

    create authorize method as a before action in application controller

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,3 +1,18 @@
 class AuthenticationController < ApplicationController
 
+    skip_before_action :authorize, only: :login
+
+    def login
+        user = User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
+         payload = {user_id: user.id}
+         secret = Rails.application.secret_key_base
+         token = JWT.encode(payload, secret)
+# also render the user along with a custom serializer. Then use useEffect to store the users info in state 
+         render json: { token: token}, status: :created
+        else
+            render json: { errors: ["Invalid username or password"] }, status: :unauthorized
+        end
+    end
+
 end
