690fae0efdb50f71ee93be602683164a035cc6f0
Carl <carlbrainegebe@gmail.com>
2021-10-04 16:49:50 -0500

    set aws s3 as storage location

--- app/serializers/exercise_serializer.rb
+++ app/serializers/exercise_serializer.rb
@@ -3,12 +3,6 @@ class ExerciseSerializer < ActiveModel::Serializer
 
   attributes :id, :name, :description, :demo_pic, :is_cardio
 
-
-  # def demo_pic
-  #   pic_info = object.demo_pic
-  #   byebug
-  # end
-
   def demo_pic
     if object.demo_pic.attached?
       url = rails_blob_path(object.demo_pic , only_path: true)
@@ -19,8 +13,6 @@ class ExerciseSerializer < ActiveModel::Serializer
       
       # set above to a variable and byebug 
     else "https://www.mcdonalds.com/content/dam/ca/nfl/web/nutrition/products/tile/en/mcdonalds-fries-small.jpg"
-      
-      
       
       # byebug 
     # return demo_pic.blob.attributes
