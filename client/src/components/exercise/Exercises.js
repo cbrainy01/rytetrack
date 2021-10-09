506f077fc4d4aee617f4a4c95adb62d3450a0df1
Carl <carlbrainegebe@gmail.com>
2021-10-09 04:20:24 -0500

    add search filter for exercises

--- client/src/components/exercise/Exercises.js
+++ client/src/components/exercise/Exercises.js
@@ -12,6 +12,7 @@ function Exercises() {
     const [exercisesPerPage, setExercisesPerPage] = useState(10)
 
     const [filterValue, setFilterValue] = useState("all")
+    const [searchValue, setSearchValue] = useState("")
     const dispatch = useDispatch()
 
     function handlePicDelete() {
@@ -21,10 +22,12 @@ function Exercises() {
     const user = useSelector(state => state.user.user)
     const exercises = useSelector(state => state.exercise.exercises)
 
-    // create sort function for exercises
+
+
     function filterExercises() {
         // make first if statement filter for a search by name
-        if(filterValue === "all") {return exercises}
+        if(searchValue !== "") { return exercises.filter( (exercise) => { if(exercise.name.toLowerCase().includes(searchValue.toLowerCase())) {return exercise} }) }
+        else if(filterValue === "all") {return exercises}
         else {return exercises.filter( (exercise) => exercise.section === filterValue )}
     }
     const filteredExercises = filterExercises()
@@ -42,10 +45,7 @@ console.log("exercises are: ", pagination())
     const pageNumbers = []
     for( let i = 1; i <= Math.ceil( filteredExercises.length / exercisesPerPage); i++ ) {pageNumbers.push(i);}
 
-    // const renderExercises = currentExercises.map((exercise) => <Exercise key={uuid()} exercise={exercise}/>)
     const renderExercises = pagination().map((exercise) => <Exercise key={uuid()} exercise={exercise}/>)
-    // const renderExercises = filteredExercises.map( (exercise) => <Exercise key={uuid()} exercise={exercise}/>)
-    // const renderExercises = filterExercises().map( (exercise) => <Exercise key={uuid()} exercise={exercise}/>)
 
     function changePage(number) {
         setCurrentPage(number)
@@ -57,6 +57,7 @@ console.log("exercises are: ", pagination())
             <h3>Exercises page</h3>
             <CreateExercise/>
 
+            <br/><input onChange={ (e) => setSearchValue(e.target.value) } placeholder="search exercises" type="text" value={searchValue}/>
             <br/><label>filter</label>
             <select onChange={(e) => setFilterValue(e.target.value)}>
                 <option value="all">all</option>
@@ -72,7 +73,6 @@ console.log("exercises are: ", pagination())
                     {pageNumbers.map( (number) => <>
                     <li key={number} >
                         <button  onClick={ () => changePage(number) }  >{number}</button>
-                        {/* <a  onClick={ () => changePage(number) } href="!#">{number}</a> */}
                     </li>
                     </> )}
                 </ul>
